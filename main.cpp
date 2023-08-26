e9a658ea13897ce326c609ba56ef40ab9c26dbb1
Awesome Kalin <47223207+AwesomeKalin@users.noreply.github.com>
2023-08-26 16:17:36 +0100

    Basic window

---
+++ main.cpp
@@ -0,0 +1,71 @@
+#include "raylib-cpp.hpp"
+
+#if defined(PLATFORM_WEB)
+    #include <emscripten/emscripten.h>
+#endif
+
+//----------------------------------------------------------------------------------
+// Global Variables Definition
+//----------------------------------------------------------------------------------
+int screenWidth = 1280;
+int screenHeight = 720;
+
+//----------------------------------------------------------------------------------
+// Module Functions Declaration
+//----------------------------------------------------------------------------------
+void UpdateDrawFrame(void);     // Update and Draw one frame
+
+//----------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------
+// Main Enry Point
+//----------------------------------------------------------------------------------
+int main()
+{
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    raylib::Window window(screenWidth, screenHeight, "Raft Away!");
+
+#if defined(PLATFORM_WEB)
+    emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
+#else
+    SetTargetFPS(60);   // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!window.ShouldClose())    // Detect window close button or ESC key
+    {
+        UpdateDrawFrame();
+    }
+#endif
+
+    return 0;
+}
+
+//----------------------------------------------------------------------------------
+// Module Functions Definition
+//----------------------------------------------------------------------------------
+void UpdateDrawFrame(void)
+{
+    // Update
+    //----------------------------------------------------------------------------------
+    // TODO: Update your variables here
+    //----------------------------------------------------------------------------------
+
+    // Draw
+    //----------------------------------------------------------------------------------
+    BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+		
+		DrawFPS(0, 0);
+		
+		const char* text = "Welcome to Raft Away";
+		int fontSize = 50;
+		int textWidth = screenWidth - MeasureText(text, fontSize);
+		int textPosWidth = textWidth / 2;
+        DrawText(text, textPosWidth, (screenHeight - fontSize) / 2, fontSize, RED);
+
+    EndDrawing();
+    //----------------------------------------------------------------------------------
+}
